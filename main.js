97e943c7431df9f9cb6a1ec152a1dde425c06f5d
orserv <61934096+orserv@users.noreply.github.com>
2023-08-27 12:51:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,6 @@
 
     var historydata = [
                         ['bing', 'https://cn.bing.com/search?q='],
-                      
                         ['google', 'https://www.google.com/#newwindow=1&safe=strict&q='],
                         ['douban', 'https://www.douban.com/search?q='],
                         ['sinaweibo', 'http://s.weibo.com/weibo/'],
